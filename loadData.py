d077a4fc495d4ba49f84c781e7b6f80b7c69ffab
lizator <frederikkoefoeds@gmail.com>
2021-03-02 16:47:01 +0100

    implemented removal of g3 0s

--- loadData.py
+++ loadData.py
@@ -19,6 +19,19 @@ absence = doc.col_values(4,1,396)
 g3 = doc.col_values(5,1,396)
 maxEdu = doc.col_values(6,1,396)
 
+index = 0
+while index != len(Female): #Removing all instances where g3 is 0
+    if g3[index] < 0.5:
+        Female = Female[:index] + Female[index+1:]
+        Male = Male[:index] + Male[index+1:]
+        Age = Age[:index] + Age[index+1:]
+        study = study[:index] + study[index+1:]
+        absence = absence[:index] + absence[index+1:]
+        g3 = g3[:index] + g3[index+1:]
+        maxEdu = maxEdu[:index] + maxEdu[index+1:]
+    else:
+        index += 1
+
 lg3=[]
 for index in g3:
     if index <7:
@@ -43,7 +56,14 @@ X = np.empty((395,7))
 for i in range(7):
     X[:,i] = np.array(doc.col_values(i,1,396)).T
 
-for row in X:
+index = 0
+while index < len(X):
+    if X[index][5] < 0.5:
+        X = np.vstack((X[:index], X[index+1:]))
+    else:
+        index += 1
+
+"""for row in X:
     if row[col] <7:
         row[col] = 0
     elif row[col] < 12:
@@ -51,8 +71,8 @@ for row in X:
     elif row[col] < 18:
         row[col] = 2
     else:
-        row[col] = 3
-    
+        row[col] = 3"""
+        
 
 # Compute values of N, M and C.
 N = len(y)
