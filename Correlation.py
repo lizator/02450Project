18216ff9bc928ca189f3081152f98bb42c91d4a2
lizator <frederikkoefoeds@gmail.com>
2021-03-02 13:02:28 +0100

    Update Correlation.py

--- Correlation.py
+++ Correlation.py
@@ -17,29 +17,38 @@ for index in range(len(Xcopy)):
     Xcopy[index] = NoiseCreator.createNoise(Xcopy[index], 0.2)
 
 figure(figsize=(24,20))
-for i in range(4):
-    for m1 in range(M):
-        for m2 in range(M):
-            subplot(M, M, m1*M + m2 + 1)
-            for c in range(C):
-                class_mask = (y==c)
-                collected_mask = []
-                for boo in class_mask:
-                    collected_mask.append(True)
-                plot(np.array(Xcopy[class_mask,m2]), np.array(Xcopy[class_mask,m1]), '.')
-                if m1==M-1:
-                    xlabel(attributeNames[m2] + ", Correlation: %.4f " % np.corrcoef(np.array(X[collected_mask,m2]), np.array(X[collected_mask,m1]))[0,1])
-                else:
-                    xticks([])
-                    xlabel("Correlation: %.4f" % np.corrcoef(np.array(X[collected_mask,m2]), np.array(X[collected_mask,m1]))[0,1])
-                if m2==0:
-                    ylabel(attributeNames[m1])
-                else:
-                    yticks([])
-                #ylim(0,X.max()*1.1)
-                #xlim(0,X.max()*1.1)
+for m1 in range(M):
+    for m2 in range(M):
+        subplot(M, M, m1*M + m2 + 1)
+        for c in range(C):
+            class_mask = (y==c)
+            collected_mask = []
+            for boo in class_mask:
+                collected_mask.append(True)
+            plot(np.array(Xcopy[class_mask,m2]), np.array(Xcopy[class_mask,m1]), '.')
+            if m1==M-1:
+                xlabel(attributeNames[m2] + ", Correlation: %.4f " % np.corrcoef(np.array(X[collected_mask,m2]), np.array(X[collected_mask,m1]))[0,1])
+            else:
+                xticks([])
+                xlabel("Correlation: %.4f" % np.corrcoef(np.array(X[collected_mask,m2]), np.array(X[collected_mask,m1]))[0,1])
+            if m2==0:
+                ylabel(attributeNames[m1])
+            else:
+                yticks([])
+            #ylim(0,X.max()*1.1)
+            #xlim(0,X.max()*1.1)
 legend(classNames)
 
 show()
 
-print('Ran Exercise 4.2.5')
+for c in range(C):
+    class_mask = (y==c)
+    collected_mask = []
+    for boo in class_mask:
+        collected_mask.append(True)
+    plot(np.array(Xcopy[class_mask,7]), np.array(Xcopy[class_mask,6]), '.')
+
+ylabel("G3")
+xlabel("maxEdu, Correlation: %.4f" % np.corrcoef(np.array(X[collected_mask,7]), np.array(X[collected_mask,6]))[0,1])
+legend(classNames)
+show()
