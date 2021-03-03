309912b1eebf37342a1b037f4a6a2e37eb6d9c0f
Adin1128 <Adin11281128@hotmail.com>
2021-03-03 15:33:54 +0100

    Updated PCA

--- PCA.py
+++ PCA.py
@@ -62,6 +62,37 @@ ylabel('PC{0}'.format(j+1))
 # Output result to screen
 show()
 
+Y = X - np.ones((N,1))*X.mean(axis=0)
+Y = Y*(1/np.std(Y,0))
+# PCA by computing SVD of Y
+U,S,V = svd(Y,full_matrices=False)
+
+# Compute variance explained by principal components
+rho = (S*S) / (S*S).sum() 
+
+
+V = V.T    
+
+# Project the centered data onto principal component space
+Z = Y @ V
+# Indices of the principal components to be plotted
+i = 0
+j = 1
+
+# Plot PCA of the data
+f = figure()
+title('NanoNose data: PCA')
+#Z = array(Z)
+for c in range(C):
+    # select indices belonging to class c:
+    class_mask = y==c
+    plot(Z[class_mask,i], Z[class_mask,j], 'o', alpha=.5)
+legend(classNames)
+xlabel('PC{0}'.format(i+1))
+ylabel('PC{0}'.format(j+1))
+
+# Output result to screen
+show()
 print('Ran Exercise 2.1.4')
 
 # exercise 2.2.4
