7ad00f76bba9f6b508a9b6526df4d826e76059ac
Mik1812 <s183913@student.dtu.dk>
2021-04-06 11:38:05 +0200

    R script added

--- Regression - Statistical Comparison.R
+++ Regression - Statistical Comparison.R
@@ -1,6 +1,6 @@
-df_ann = read.csv("/Users/mikkeldanielsen/PycharmProjects/02450Project/Error_ANN.csv", header = FALSE)
-df_base = read.csv("/Users/mikkeldanielsen/PycharmProjects/02450Project/Error_baseline.csv", header = FALSE)
-df_lr = read.csv("/Users/mikkeldanielsen/PycharmProjects/02450Project/Error_linear_regression.csv", header = FALSE)
+df_ann = read.csv("Error_ANN.csv", header = FALSE)
+df_base = read.csv("Error_baseline.csv", header = FALSE)
+df_lr = read.csv("Error_linear_regression.csv", header = FALSE)
 
 ann_vs_lr = df_ann-df_lr
 ann_vs_base = df_ann-df_base
