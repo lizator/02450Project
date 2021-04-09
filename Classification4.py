6db18342c3a2c130aa2e7fce1a8ebed1d6a54576
lizator <frederikkoefoeds@gmail.com>
2021-04-09 17:48:41 +0200

    comments

--- Classification4.py
+++ Classification4.py
@@ -1,16 +1,19 @@
 import numpy as np
 import pickle
 
+
+# loading estimates
+
 pickle_a = open("ClassificationData/a.pickle", "rb")
 pickle_b = open("ClassificationData/b.pickle", "rb")
 pickle_c = open("ClassificationData/c.pickle", "rb") 
 
-y_est_a = pickle.load(pickle_a)    
-y_est_b = pickle.load(pickle_b)  
-y_est_c = pickle.load(pickle_c)
+y_est_a = pickle.load(pickle_a)  # K-Nearest
+y_est_b = pickle.load(pickle_b)  # Logistic Regression
+y_est_c = pickle.load(pickle_c)  # BaseLine
 
 pickle_a.close()
 pickle_b.close()
 pickle_c.close()
 
-
+# loading done
