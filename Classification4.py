b08dd7f739688905a51a05d7a32c0706285efd12
Adin1128 <Adin11281128@hotmail.com>
2021-04-17 15:27:04 +0200

    Lamdas in main and mcnemar comparison

--- Classification4.py
+++ Classification4.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pickle
-
+from toolbox_02450 import mcnemar
+from rmG3 import *
 
 # loading estimates
 
@@ -15,5 +16,12 @@ y_est_c = pickle.load(pickle_c)  # BaseLine
 pickle_a.close()
 pickle_b.close()
 pickle_c.close()
+
+
+# Compute the mcnemar's interval
+alpha = 0.05
+[thetahat, CI, p] = mcnemar(y, y_est_a, y_est_b, alpha=alpha)
+[thetahat, CI, p] = mcnemar(y, y_est_a, y_est_c, alpha=alpha)
+[thetahat, CI, p] = mcnemar(y, y_est_b, y_est_c, alpha=alpha)
 
 # loading done
